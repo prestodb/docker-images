bcf7b97533c505056aadf34e6dcb197ba6567dea
Naveen Kumar Mahadevuni <nmahadevuni@gmail.com>
2021-05-26 14:38:11 +0530

    Add Apache 3.1.2
    Cherry pick of https://github.com/trinodb/docker-images/pull/79
    
    Co-authored-by: Guy Cohen <guyc@gett.com>

---
+++ prestodb/hive3.1-hive/files/etc/hadoop-init.d/set-aws-creds.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+if [[ -n "${AWS_ACCESS_KEY_ID}" ]]
+then
+    echo "Setting AWS keys"
+    sed -i  -e "s|\"Use AWS_ACCESS_KEY_ID .*\"|${AWS_ACCESS_KEY_ID}|g" \
+            -e "s|\"Use AWS_SECRET_ACCESS_KEY .*\"|${AWS_SECRET_ACCESS_KEY}|g" \
+            /opt/hive/conf/hive-site.xml
+fi
