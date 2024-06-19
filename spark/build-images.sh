9e336af1ebeb68031907d84c07a01b65b308ff91
Archy-X <63976867+Archy-X@users.noreply.github.com>
2024-06-19 15:58:53 -0700

    Add missing colon

--- spark/build-images.sh
+++ spark/build-images.sh
@@ -6,5 +6,5 @@ VERSION=3.5.1
 
 docker build -t spark-base:$VERSION ./spark-base
 docker build -t spark-master:$VERSION ./spark-master
-docker build -t spark-worker$VERSION ./spark-worker
+docker build -t spark-worker:$VERSION ./spark-worker
 docker build -t spark-submit:$VERSION ./spark-submit
